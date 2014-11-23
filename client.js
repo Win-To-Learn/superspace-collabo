06c1a6295388c6fd8e6756aea72d83dd4d94638f
wintolearn <jmartin>
2014-11-22 23:21:01 -0500

    changed url back to aequoreagames.com

--- client.js
+++ client.js
@@ -58,8 +58,8 @@ var Client = IgeClass.extend({
 					// than before the scene etc are created... maybe you want
 					// a splash screen or a menu first? Then connect after you've
 					// got a username or something?
-					//var serverUrl = 'http://aequoreagames.com:7610'; // This is the url for remote deployment
-					var serverUrl = 'http://localhost:7610';
+					var serverUrl = 'http://aequoreagames.com:7610'; // This is the url for remote deployment
+					//var serverUrl = 'http://localhost:7610';
 					//var serverUrl = 'http://starcoder3.jit.su/';
 					//var serverUrl = 'http://superspace.mayumi.fi:7610'; // This is the url for remote deployment
 					//if(location.origin == "file://" || location.origin.indexOf("http://localhost") == 0) {
